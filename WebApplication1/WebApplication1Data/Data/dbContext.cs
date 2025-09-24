a9beb47cc5bc35580478c144142c9fa83e787671
Polyukhovich Andriy <kingg9940@gmail.com>
2025-09-25 00:02:11 +0300

    lab1

---
+++ WebApplication1/WebApplication1Data/Data/dbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApplication1Data.Data
+{
+    public class dbContext : IdentityDbContext<User>
+    {
+        public dbContext(DbContextOptions<dbContext> options)
+        : base(options)
+        {
+        }
+
+    }
+}
